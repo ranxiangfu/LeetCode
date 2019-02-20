9d2762ecc7c5535b0361db9fdcb87a06a1ab47ae
ranxiangfu <31944996+ranxiangfu@users.noreply.github.com>
2019-02-20 11:58:10 +0800

    Create 0001_TwoSum.java

---
+++ 0001_TwoSum.java
@@ -0,0 +1,21 @@
+/**
+ * 144 ms
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i = 0; i<nums.length; i++){
+            for(int j =0; j<nums.length; j++){
+                if(i == j){
+                    continue;
+                }
+                if(target == nums[i] + nums[j]){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
