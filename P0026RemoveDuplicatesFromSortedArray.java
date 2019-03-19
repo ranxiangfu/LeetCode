932a9276d8d67c91489cda037ffb6686ace44f8e
ranxiangfu <31944996+ranxiangfu@users.noreply.github.com>
2019-03-19 15:08:53 +0800

    Create P0026RemoveDuplicatesFromSortedArray.java

---
+++ P0026RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+/**
+ * 5 ms	42.2 MB
+ */
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[index]) {
+                nums[++index] = nums[i];
+            }
+        }
+        return ++index;
+    }
+}
