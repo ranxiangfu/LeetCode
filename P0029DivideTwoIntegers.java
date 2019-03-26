22fb614b2f4cf279eef824074333e6e0d41f7562
ranxiangfu <31944996+ranxiangfu@users.noreply.github.com>
2019-03-26 12:27:59 +0800

    Create P0029DivideTwoIntegers.java

---
+++ P0029DivideTwoIntegers.java
@@ -0,0 +1,21 @@
+/**
+ * 14 ms	38.3 MB
+ */
+class Solution {
+    public int divide(int dividend, int divisor) {
+        boolean isNegative = (dividend^divisor) < 0 ? true : false;
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+        long result = 0;
+        while(absDividend >= absDivisor){
+            long tmp = absDivisor, count = 1;
+            while(tmp <= absDividend){
+                tmp <<= 1;
+                count <<= 1;
+            }
+            result += count >> 1;
+            absDividend -= tmp >> 1;
+        }
+        return  isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
+    }
+}
