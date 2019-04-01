bdef7a560418ee702dd5390a189a018d5f784b70
ranxiangfu <31944996+ranxiangfu@users.noreply.github.com>
2019-04-01 18:59:35 +0800

    Create P0035SearchInsertPosition.java

---
+++ P0035SearchInsertPosition.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums.length == 0 || target < nums[0]) {
+            return 0;
+        }
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (l == r - 1 || l == r) {
+                return r;
+            }
+            if (nums[mid] > target) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        return 0;
+    }
+}
